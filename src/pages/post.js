fb4ba870ca1476a2d6942584bdca7036dc8c55af
pablo <cesarion23@gmail.com>
2018-04-25 17:49:48 -0400

    initializer

---
+++ src/pages/post.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import posts from '../data/posts';
+import Menu from '../components/menu';
+class Post extends Component {
+  render() {
+    const { match } = this.props;
+    let post = posts.filter(post => post.id === parseInt(match.params.postId));
+    post = post[0];
+
+    return (
+      <section>
+        <Menu />
+        <div className="post-section">
+          <h2>{post.title}</h2>
+          <p>{post.content}</p>
+        </div>
+      </section>
+    )
+  }
+}
+
+export default Post;
